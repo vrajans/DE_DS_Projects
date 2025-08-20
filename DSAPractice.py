20e41274124f451e599590d37859446e3381d15a
vrajans <varatha.51@gmail.com>
2025-08-21 01:03:08 +0530

    changes yesterday# This is a combination of 2 commits.

--- DSAPractice.py
+++ DSAPractice.py
@@ -21,5 +21,25 @@ def reverse_arr(arr):
             arr[arr_len] = val
     return arr
 
-print(reverse_arr([1, 2, 3, 4,7,9]))
+# def removeDuplicates(nums):
+#     nums_new = []
+#     for val in nums:
+#         if val not in nums_new:
+#             nums_new.append(val)
+
+#     return len(nums_new), nums_new
+
+def removeDuplicates(nums):
+    if not nums:
+        return 0
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return i + 1, nums[:i+1]
+
+print(removeDuplicates([1,1,2]))
+
+#print(reverse_arr([1, 2, 3, 4,7,9]))
 
